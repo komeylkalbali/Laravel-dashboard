2b475d1c7c044eb2294ce08fd73677fcff061b81
Komeyl-dev <komeyl.kalbali.dev@gmail.com>
2020-04-26 19:49:35 +0430

    issus breadcrumbs

--- vendor/davejamesmiller/laravel-breadcrumbs/views/bootstrap4.blade.php
+++ vendor/davejamesmiller/laravel-breadcrumbs/views/bootstrap4.blade.php
@@ -4,9 +4,9 @@
         @foreach ($breadcrumbs as $breadcrumb)
 
             @if ($breadcrumb->url && !$loop->last)
-                <li class="breadcrumb-item"><a href="{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a></li>
+                <li class="breadcrumb-item"><a href="{{ $breadcrumb->url }}">{!! $breadcrumb->title !!}</a></li>
             @else
-                <li class="breadcrumb-item active">{{ $breadcrumb->title }}</li>
+                <li class="breadcrumb-item active">{!! $breadcrumb->title !!}</li>
             @endif
 
         @endforeach
